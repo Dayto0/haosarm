32ba7c271a05e950b514dde15511d2bee06ad5f3
Dayto <89776138+Dayto0@users.noreply.github.com>
2024-12-17 00:47:32 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,20 @@
-self.addEventListener('install', event => {    event.waitUntil(        caches.open('dts-cache').then(cache => {            return cache.addAll([                '/',                '/manifest.json',                '/styles.css'            ]);        })    );});self.addEventListener('fetch', event => {    event.respondWith(        caches.match(event.request).then(response => {            return response || fetch(event.request);        })    );});
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('dts-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/manifest.json',
+                '/styles.css',
+                '/index.html'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
